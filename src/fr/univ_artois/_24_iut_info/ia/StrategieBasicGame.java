a2127086afe7a89e2ff9176846d0e8525b5e0fa3
xorgam <v.caffiaux@laposte.net>
2015-05-29 17:27:10 +0200

    strat

--- src/fr/univ_artois/_24_iut_info/ia/StrategieBasicGame.java
+++ src/fr/univ_artois/_24_iut_info/ia/StrategieBasicGame.java
@@ -1,14 +1,31 @@
 package fr.univ_artois._24_iut_info.ia;
 
+import fr.univ_artois._24_iut_info.game.Game;
 import fr.univ_artois._24_iut_info.game.Map;
 
 public class StrategieBasicGame extends AbstractStrategieGame {
 
-	public StrategieBasicGame(Map m) {
-		super(m);
+	public StrategieBasicGame(Game g) {
+		super(g);
 		// TODO Stub du constructeur généré automatiquement
 	}
 
+	
+	private void playTurn()
+	{
+		int nbPions = game.getMap().getNbPions(); //TODO a initialiser avec la methode de map
+		// Tour 1
+		if(nbPions == 0)
+		{
+			// jouer les tour 1
+			
+		}
+		
+		else
+		{ 
+			// Tour x
+		}
+	}
 	
 	
 	
