48a876e8c4ea086fa5076e44b7cc605a29f30fa4
antoinebacquet <antoinebacquet1@gmail.com>
2015-05-29 17:55:19 +0200

    wtf

--- src/fr/univ_artois/_24_iut_info/ia/AbstractStrategieGame.java
+++ src/fr/univ_artois/_24_iut_info/ia/AbstractStrategieGame.java
@@ -3,25 +3,14 @@ package fr.univ_artois._24_iut_info.ia;
 import java.util.List;
 
 import fr.univ_artois._24_iut_info.game.Game;
-import fr.univ_artois._24_iut_info.game.Map;
 
 public abstract class AbstractStrategieGame {
-<<<<<<< HEAD
 	
 	protected Game game;
-	
-=======
-	protected Game game;
->>>>>>> branch 'master' of https://github.com/marcbal/24IUTInfoProg
 	protected List<TurnPossibility> nextTurnPossibilities;
-<<<<<<< HEAD
 	
 	public AbstractStrategieGame(Game game){
 		this.game =  game;
-=======
-	public AbstractStrategieGame(Game g){
-		game = g;
->>>>>>> branch 'master' of https://github.com/marcbal/24IUTInfoProg
 	}
 	
 }
