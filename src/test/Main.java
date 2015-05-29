45aed45d901a8789ad8ce6abd2e80fa38646bfc1
xorgam <v.caffiaux@laposte.net>
2015-05-29 13:26:07 +0200

    #test2

--- src/test/Main.java
+++ src/test/Main.java
@@ -3,7 +3,7 @@ package test;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Coucou tout le monde");
+		System.out.println("Si on a un conflit, n'oublie pas qui a gagné la guerre d'algerie ! ");
 	}
 
 }
