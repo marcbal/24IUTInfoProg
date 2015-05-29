53141d6083ddb35bf60269af6fa1de74652c5a2a
antoinebacquet <antoinebacquet1@gmail.com>
2015-05-29 15:19:47 +0200

    paramettre pour les joueurs

--- src/fr/univ_artois/_24_iut_info/game/PlayerHuman.java
+++ src/fr/univ_artois/_24_iut_info/game/PlayerHuman.java
@@ -7,6 +7,10 @@ public class PlayerHuman extends Player {
 	
 	
 	
+	public PlayerHuman(int nbTwist, Game game) {
+		super(nbTwist, game);
+	}
+
 	@Override
 	public void play() {
 		System.out.println("10-A vous de jouer ("+this.id+")");
