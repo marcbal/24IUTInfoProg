7fe05bdb10c5c071ccf9b7dfba520cc74ef214de
Marc Baloup <marc.baloup@laposte.net>
2015-05-29 13:35:35 +0200

    Correction du bug d'encodage dans le fichier test

--- src/test/Main.java
+++ src/test/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		System.out.println("Si on a un conflit, n'oublie pas qui a gagn� la guerre d'algerie ! ");
+		System.out.println("Si on a un conflit, n'oublie pas qui a gagné la guerre d'algerie ! ");
 	}
 
 }
