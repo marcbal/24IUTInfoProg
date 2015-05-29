3f263379317d1f8937882f7da088b2b2e92185f0
antoinebacquet <antoinebacquet1@gmail.com>
2015-05-29 14:26:51 +0200

    Creation des class pour le player

---
+++ src/fr/univ_artois/_24_iut_info/game/PLayerIA.java
@@ -0,0 +1,5 @@
+package fr.univ_artois._24_iut_info.game;
+
+public class PLayerIA extends Player {
+
+}
---
+++ src/fr/univ_artois/_24_iut_info/game/Player.java
@@ -0,0 +1,10 @@
+package fr.univ_artois._24_iut_info.game;
+
+public abstract class Player {
+	
+	
+	
+	
+	public abstract void play();
+
+}
