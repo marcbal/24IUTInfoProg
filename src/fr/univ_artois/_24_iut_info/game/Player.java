4a92fb218ab5c7caaabbaba06b1abaf98a1125eb
antoinebacquet <antoinebacquet1@gmail.com>
2015-05-29 14:32:24 +0200

    id pour les joueurs

--- src/fr/univ_artois/_24_iut_info/game/Player.java
+++ src/fr/univ_artois/_24_iut_info/game/Player.java
@@ -3,7 +3,17 @@ package fr.univ_artois._24_iut_info.game;
 public abstract class Player {
 	
 	
+
+	private static int ID_COUNT = 1;
 	
+	protected int id;
+	
+	public Player(){
+		this.id = this.ID_COUNT;
+		this.ID_COUNT ++ ;
+		
+		
+	}
 	
 	public abstract void play();
 
--- src/fr/univ_artois/_24_iut_info/game/PlayerHuman.java
+++ src/fr/univ_artois/_24_iut_info/game/PlayerHuman.java
@@ -2,9 +2,14 @@ package fr.univ_artois._24_iut_info.game;
 
 public class PlayerHuman extends Player {
 
+	
+	
+	
+	
+	
 	@Override
 	public void play() {
-		// TODO Auto-generated method stub
+		System.out.println("");
 
 	}
 
