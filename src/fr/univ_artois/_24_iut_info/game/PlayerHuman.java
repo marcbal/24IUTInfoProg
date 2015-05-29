4435f6c21bce8c150fa01407bf9944ab4848e899
Marc Baloup <marc.baloup@laposte.net>
2015-05-29 17:50:34 +0200

    Développement du joueur humain

--- src/fr/univ_artois/_24_iut_info/game/PlayerHuman.java
+++ src/fr/univ_artois/_24_iut_info/game/PlayerHuman.java
@@ -1,8 +1,10 @@
 package fr.univ_artois._24_iut_info.game;
 
+import java.util.Scanner;
+
 public class PlayerHuman extends Player {
 
-	
+	Scanner keyboard = new Scanner(System.in);
 
 	
 	public PlayerHuman(int nbTwist, Game game, int id) {
@@ -11,9 +13,21 @@ public class PlayerHuman extends Player {
 
 	@Override
 	public void play() {
-		System.out.println("10-A vous de jouer ("+this.id+")");
+		System.out.println("Coordonnée où positionner le twist-lock (exemple : \"0 5\")");
 		
-
+		try {
+			String in = keyboard.nextLine();
+			
+			String[] coordsStr = in.split(" ");
+			
+	
+			sendTurn(Integer.parseInt(coordsStr[0]), Integer.parseInt(coordsStr[1]));
+			
+		} catch(Exception e) {
+			System.err.println("Erreur de saisie, on envoie 0 0 au serveur");
+			sendTurn(0, 0);
+		}
+		
 	}
 
 }
