85c9f2b59b501e721e0b9fb693d20f76f42e29d2
Babanar <louis.behague@orange.fr>
2015-05-29 18:03:35 +0200

    Les points capturés

--- src/fr/univ_artois/_24_iut_info/ia/AbstractStrategieGame.java
+++ src/fr/univ_artois/_24_iut_info/ia/AbstractStrategieGame.java
@@ -3,15 +3,18 @@ package fr.univ_artois._24_iut_info.ia;
 import java.util.List;
 
 import fr.univ_artois._24_iut_info.game.Game;
+import fr.univ_artois._24_iut_info.game.Map;
 
 public abstract class AbstractStrategieGame {
 
 	
 	protected Game game;
-	protected List<TurnPossibility> nextTurnPossibilities;
-	
-	public AbstractStrategieGame(Game game){
-		this.game =  game;
+
+	protected Map map;
+	protected List<List<TurnPossibility>> nextTurnPossibilities;
+	public AbstractStrategieGame(Game g){
+		map = g.getMap();
+		game = g;
 	}
 	
 	public abstract void playTurn();
